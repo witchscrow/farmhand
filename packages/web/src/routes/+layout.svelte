e8c6041f7a257ddf7fc4e783d0a59343e0f3db1c
Zachary Corvidae <zach@sneakycrow.dev>
2024-10-30 14:57:13 -0700

    Add dark mode with ThemeToggler

--- packages/web/src/routes/+layout.svelte
+++ packages/web/src/routes/+layout.svelte
@@ -1,6 +1,14 @@
 <script lang="ts">
 	import '../app.css';
+	import ThemeToggler from '$lib/components/ThemeToggler.svelte';
 	let { children } = $props();
 </script>
 
-{@render children()}
+<main
+	class="min-w-screen dark:bg-secondary-950 bg-secondary-50 dark:text-primary-100 text-primary-950 min-h-screen text-base"
+>
+	<div class="fixed right-4 top-4 z-50">
+		<ThemeToggler />
+	</div>
+	{@render children()}
+</main>
--- packages/web/src/routes/+page.svelte
+++ packages/web/src/routes/+page.svelte
@@ -1,2 +1,3 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<section class="flex items-center justify-center p-4">
+	<h1 class="text-4xl">farmhand</h1>
+</section>
