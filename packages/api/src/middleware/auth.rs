b9128e33c4ac8fc7746b319097a00f813775982a
Zachary Corvidae <zach@sneakycrow.dev>
2024-10-31 16:02:08 -0700

    Have auth middleware pass user data

--- packages/api/src/middleware/auth.rs
+++ packages/api/src/middleware/auth.rs
@@ -1,19 +1,30 @@
-use crate::jwt::decode_jwt;
+use std::sync::Arc;
+
+use crate::{jwt::decode_jwt, AppState};
 use axum::{
     body::Body,
-    extract::Request,
+    extract::{Request, State},
     http::{self, Response, StatusCode},
     middleware::Next,
 };
+use db::users::User;
+use uuid::Uuid;
 
 /// A middleware for checking the validity of the JWT token
-pub async fn auth_middleware(mut req: Request, next: Next) -> Result<Response<Body>, StatusCode> {
+pub async fn auth_middleware(
+    State(state): State<Arc<AppState>>,
+    mut req: Request,
+    next: Next,
+) -> Result<Response<Body>, StatusCode> {
     // Get the auth header from the request
     let raw_auth_header = req.headers_mut().get(http::header::AUTHORIZATION);
     // Pull the full header string out of the header
     let auth_header = match raw_auth_header {
         Some(header) => header.to_str().map_err(|_| StatusCode::BAD_REQUEST),
-        None => return Err(StatusCode::BAD_REQUEST),
+        None => {
+            tracing::error!("Could not get authorization from header");
+            return Err(StatusCode::BAD_REQUEST);
+        }
     }?;
     // Full header is expected to be `Bearer token`, split by whitespace
     let mut split_header = auth_header.split_whitespace();
@@ -22,8 +33,22 @@ pub async fn auth_middleware(mut req: Request, next: Next) -> Result<Response<Bo
     let jwt_token = token.expect("Could not parse token").to_owned();
     let token_claims = match decode_jwt(jwt_token) {
         Ok(token) => token,
-        Err(jwt_err) => return Err(StatusCode::UNAUTHORIZED),
+        Err(jwt_err) => {
+            tracing::error!("Error decoding jwt {jwt_err:?}");
+            return Err(StatusCode::UNAUTHORIZED);
+        }
     };
-    // TODO: Using the token_claims, get the User and add it to the request for downstream usage
+    // Convert the user id from a string to a uuid
+    let user_id = Uuid::parse_str(&token_claims.claims.user_id).map_err(|e| {
+        tracing::error!("Could not parse user id from token to uuid {e}");
+        StatusCode::BAD_REQUEST
+    })?;
+    // Get the users data from the token
+    let user = User::from_id(user_id, &state.db).await.map_err(|e| {
+        tracing::error!("Could not get user from database in middleware {e}");
+        StatusCode::INTERNAL_SERVER_ERROR
+    })?;
+    // Pass the user to the extensions
+    req.extensions_mut().insert(user);
     Ok(next.run(req).await)
 }
