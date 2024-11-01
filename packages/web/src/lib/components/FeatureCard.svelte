49d591eacccbaa7123bdb8059611c717591242a1
Zachary Corvidae <zach@sneakycrow.dev>
2024-11-01 11:01:46 -0700

    Make primary colors darker

--- packages/web/src/lib/components/FeatureCard.svelte
+++ packages/web/src/lib/components/FeatureCard.svelte
@@ -4,9 +4,11 @@
 	export let icon: string;
 </script>
 
-<div class="rounded-lg border border-gray-200 p-6 dark:border-gray-700">
-	<div class="mb-4 text-3xl">{icon}</div>
-	<h3 class="mb-2 text-lg font-semibold text-primary-700 dark:text-primary-200">
+<div
+	class="rounded-sm border-4 border-secondary-600 bg-secondary-100/90 p-6 text-left shadow-sm shadow-primary-800 dark:border-primary-600 dark:bg-primary-800/90"
+>
+	<h3 class="mb-2 text-secondary-800 dark:text-white">
+		<span class="text-xs">{icon}</span>
 		{title}
 	</h3>
 	<p class="text-sm text-gray-600 dark:text-gray-300">
