34c50196aadec471ac67c09b7c05b7c6c3168047
Zachary Corvidae <zach@sneakycrow.dev>
2024-11-09 04:18:20 -0800

    Change video ids to use nanoid

--- packages/db/src/videos.rs
+++ packages/db/src/videos.rs
@@ -1,10 +1,11 @@
+use nanoid::nanoid;
 use serde::{Deserialize, Serialize};
-use sqlx::{postgres::PgRow, prelude::FromRow, PgPool, Row};
+use sqlx::{prelude::FromRow, PgPool};
 use uuid::Uuid;
 
 #[derive(Debug, Serialize, Deserialize, FromRow)]
 pub struct Video {
-    pub id: Uuid,
+    pub id: String,
     pub user_id: Uuid,
     pub title: String,
     pub raw_video_path: String,
@@ -30,30 +31,20 @@ impl Video {
         title: String,
         raw_video_path: String,
     ) -> Result<Self, sqlx::Error> {
-        let row = sqlx::query(
+        let video_id = nanoid!(10);
+        sqlx::query_as::<_, Video>(
             r#"
-            INSERT INTO videos (user_id, title, raw_video_path, processing_status)
-            VALUES ($1, $2, $3, 'pending')
+            INSERT INTO videos (id, user_id, title, raw_video_path, processing_status)
+            VALUES ($1, $2, $3, $4, 'pending')
             RETURNING id, user_id, title, raw_video_path, processed_video_path,
                       processing_status, created_at, updated_at
             "#,
         )
+        .bind(video_id)
         .bind(user_id)
         .bind(title)
         .bind(raw_video_path)
-        .map(|row: PgRow| Video {
-            id: row.get("id"),
-            user_id: row.get("user_id"),
-            title: row.get("title"),
-            raw_video_path: row.get("raw_video_path"),
-            processed_video_path: row.get("processed_video_path"),
-            processing_status: row.get("processing_status"),
-            created_at: row.get("created_at"),
-            updated_at: row.get("updated_at"),
-        })
         .fetch_one(pool)
-        .await?;
-
-        Ok(row)
+        .await
     }
 }
