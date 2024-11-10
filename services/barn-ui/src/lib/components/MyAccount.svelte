287b8ea1826f662da32d56db2682251af79204c7
Zachary Corvidae <zach@sneakycrow.dev>
2024-11-09 23:37:03 -0800

    Fix styles for Form and MyAccount

--- services/barn-ui/src/lib/components/MyAccount.svelte
+++ services/barn-ui/src/lib/components/MyAccount.svelte
@@ -27,28 +27,27 @@
 	</div>
 	<div
 		data-popup="my-account"
-		class="z-20 min-w-52 rounded border-2 border-primary-800 bg-primary-900"
+		class="divide z-20 min-w-52 divide-y-2 divide-primary-900 rounded border-2 border-primary-700 bg-primary-800 shadow-xl"
 	>
-		<aside class="px-6 py-4 text-primary-50 dark:text-white">
+		<aside class="flex flex-col items-start px-6 py-4 text-primary-50 dark:text-white">
 			<p class="text-xs font-medium">Signed in as</p>
 			<p class="text-lg font-semibold">{user.username}</p>
 			<p class="text-base text-primary-200 dark:text-primary-200">{user.email}</p>
 		</aside>
-		<hr class="border-primary-800" />
 		<ul class="p-2">
 			<li>
 				<a
 					href="/account"
-					class="flex w-full flex-nowrap items-center rounded px-4 py-2 font-semibold text-primary-50 hover:bg-primary-800 dark:text-white"
+					class="flex w-full flex-nowrap items-center rounded px-4 py-2 font-semibold text-primary-50 hover:bg-primary-700 dark:text-white"
 				>
 					<MyAccount class="mr-2" />
 					<span>My Account</span>
 				</a>
 			</li>
 			<li>
-				<form action="?/logout" method="POST" use:enhance>
+				<form action="/?/logout" method="POST" use:enhance>
 					<button
-						class="flex w-full flex-nowrap items-center rounded px-4 py-2 font-semibold text-primary-50 hover:bg-primary-800 dark:text-white"
+						class="flex w-full flex-nowrap items-center rounded px-4 py-2 font-semibold text-primary-50 hover:bg-primary-700 dark:text-white"
 					>
 						<SignOut class="mr-2" />
 						<span>Sign out</span>
@@ -57,14 +56,4 @@
 			</li>
 		</ul>
 	</div>
-
-	<style>
-		#menu {
-			display: none;
-			width: max-content;
-			position: absolute;
-			top: 0;
-			left: 0;
-		}
-	</style>
 {/if}
