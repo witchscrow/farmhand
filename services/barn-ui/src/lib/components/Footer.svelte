f37aecef2566e4c28bc4b347978ef38949862af2
Zachary Corvidae <zach@sneakycrow.dev>
2024-11-16 11:52:55 -0800

    Make header and footer more flexible

--- services/barn-ui/src/lib/components/Footer.svelte
+++ services/barn-ui/src/lib/components/Footer.svelte
@@ -1,4 +1,8 @@
-<footer class="w-full py-8 text-black dark:text-white">
+<script lang="ts">
+	let { class: className }: { class?: string } = $props();
+</script>
+
+<footer class="w-full py-8 text-black dark:text-white {className}">
 	<div class="container mx-auto max-w-screen-lg px-4">
 		<div class="mt-8 pt-8 text-center text-sm">
 			<p>
