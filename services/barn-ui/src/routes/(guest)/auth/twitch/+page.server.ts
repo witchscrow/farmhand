e1fa2ac265a265546085f51374f3c9c1eba9f202
Zachary Corvidae <zach@sneakycrow.dev>
2024-12-01 17:40:31 -0800

    Fix env import

--- services/barn-ui/src/routes/(guest)/auth/twitch/+page.server.ts
+++ services/barn-ui/src/routes/(guest)/auth/twitch/+page.server.ts
@@ -1,7 +1,7 @@
 import { redirect } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
-import { API_URL } from '$env/static/private';
+import { env } from '$env/dynamic/private';
 
 export const load = (async () => {
-	redirect(307, `${API_URL}/auth/twitch`);
+	redirect(307, `${env.API_URL}/auth/twitch`);
 }) satisfies PageServerLoad;
