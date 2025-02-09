dd5af52a6bc9ff4831ab3cf6c64f135efeedb706
Zachary Corvidae <zach@sneakycrow.dev>
2025-02-09 13:47:24 -0800

    Add Twitch Integration section to My Account

--- services/barn-ui/src/routes/(user)/me/+page.svelte
+++ services/barn-ui/src/routes/(user)/me/+page.svelte
@@ -17,7 +17,9 @@
 		>
 			<span class="text-xl">Welcome</span> <span class="uppercase">{user.username}</span>
 		</h1>
-		<p>Manage your account here</p>
+		<p class="text-lg font-semibold text-primary-700 dark:text-primary-100">
+			Manage your account here
+		</p>
 	</aside>
 	<Card>
 		<div slot="header">
@@ -39,5 +41,52 @@
 				>
 			</div>
 		</div>
+	</Card>
+	<Card>
+		<div slot="header">
+			<h3 class="font-serif text-lg text-primary-700 dark:text-primary-100">Twitch Integration</h3>
+			<p class="text-sm text-primary-400 dark:text-primary-200">
+				Configure your Twitch API connections
+			</p>
+		</div>
+		<form slot="content" class="flex flex-col space-y-4">
+			<div class="flex items-center justify-between">
+				<div>
+					<label for="toggleStream" class="font-semibold text-primary-700 dark:text-white">
+						Stream Status
+						<p class="text-sm text-primary-400">Track when stream starts and stops</p>
+					</label>
+				</div>
+				<input type="checkbox" class="checkbox checked:border-primary-500" name="streamStatus" />
+			</div>
+			<div class="flex items-center justify-between">
+				<div>
+					<label for="toggleChat" class="font-semibold text-primary-700 dark:text-white">
+						Chat Messages
+						<p class="text-sm text-primary-400">Access Twitch chat messages</p>
+					</label>
+				</div>
+				<input type="checkbox" class="checkbox checked:border-primary-500" name="chatMessages" />
+			</div>
+			<div class="flex items-center justify-between">
+				<div>
+					<label for="togglePoints" class="font-semibold text-primary-700 dark:text-white">
+						Channel Points
+						<p class="text-sm text-primary-400">Manage channel point rewards</p>
+					</label>
+				</div>
+				<input type="checkbox" class="checkbox checked:border-primary-500" name="channelPoints" />
+			</div>
+			<div class="flex items-center justify-between">
+				<div>
+					<label for="toggleSubs" class="font-semibold text-primary-700 dark:text-white">
+						Follows & Subs
+						<p class="text-sm text-primary-400">Track follower and subscriber events</p>
+					</label>
+				</div>
+				<input type="checkbox" class="checkbox checked:border-primary-500" name="followsSubs" />
+			</div>
+			<button class="variant-filled-primary btn w-full">Save Integration Settings</button>
+		</form>
 	</Card>
 </section>
